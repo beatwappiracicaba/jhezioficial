9dfef1e9241b9a3eb56229d80d51c3bc298d509f
BEATWAP <beatwappiracicaba@gmail.com>
2026-06-30 15:41:55 -0300

    supa atua

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  return defineConfig({
+    plugins: [react()],
+    envPrefix: ['VITE_'],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+    },
+    define: {
+      'process.env': {
+        ...env,
+      },
+    },
+  });
+};
